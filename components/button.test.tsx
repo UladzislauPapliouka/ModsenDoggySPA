ea88da5b573e0c8d8fba63d06f119ff79cb8a975
Uladzislau Papliouka <uladzislau.papliouka@gmail.com>
2024-08-12 16:12:23 +0300

    feat(test): init jest tests

---
+++ components/button.test.tsx
@@ -0,0 +1,13 @@
+import '@testing-library/jest-dom';
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import { Button } from '@components/button';
+import { describe } from 'node:test';
+
+describe('Button component test', () => {
+  it('render button', () => {
+    render(<Button />);
+    const button = screen.getByText('Click');
+    expect(button).toBeInTheDocument();
+  });
+});
