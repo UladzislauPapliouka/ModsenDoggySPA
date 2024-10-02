e3d796cc44586f04ab37fdad6dd5cf3d4ffe57b3
Uladzislau Papliouka <uladzislau.papliouka@gmail.com>
2024-10-02 22:12:04 +0300

    feat(scss): use scss variables

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,10 +2,10 @@ import { Header } from '@components/Header';
 import { FC, PropsWithChildren } from 'react';
 export const Layout: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <>
+    <div>
       <Header />
       <main>{children}</main>
       <footer>Footer</footer>
-    </>
+    </div>
   );
 };
