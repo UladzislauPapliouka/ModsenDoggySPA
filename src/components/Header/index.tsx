73644ff9a19e212fd81fbb248acdc41b3fe7c276
Uladzislau Papliouka <uladzislau.papliouka@gmail.com>
2025-01-19 21:26:44 +0300

    feat(refactoring): refactor header styles and add scroll hide prototype

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,9 @@ import { usePathname } from 'next/navigation';
 import { NavLink } from './NavLink';
 import style from './index.module.scss';
 import { routes } from 'shared/routes';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import Image from 'next/image';
+import logo from '../../assets/logo.png';
 export const Header = () => {
   const currentPath = usePathname();
   const [isOpen, setIsOpen] = useState(false);
@@ -17,8 +19,76 @@ export const Header = () => {
       removeEventListener('resize', onResize);
     };
   }, []);
+  const scrolledFromTop = useRef(0);
+  const currentScrollDistance = useRef(0);
+  const scrollDirection = useRef('down');
+  const blockRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > scrolledFromTop.current) {
+        scrollDirection.current = 'down';
+      } else if (window.scrollY < scrolledFromTop.current) {
+        scrollDirection.current = 'up';
+      }
+      currentScrollDistance.current = Math.abs(
+        scrolledFromTop.current - window.scrollY
+      );
+      // console.log('scrollDirection down', scrollDirection.current === 'down');
+      // console.log(
+      //   'currentScrollDistance > 100',
+      //   currentScrollDistance.current > 100
+      // );
+      // console.log('window.innerWidth < 748', window.innerWidth < 748);
+      // console.log(
+      //   " !blockRef!.current!.getAttribute('data-is-open')",
+      //   blockRef!.current!.getAttribute('data-is-open')
+      // );
+      // console.log(
+      //   " blockRef!.current!.getAttribute('data-is-open')",
+      //   blockRef!.current!.getAttribute('data-is-open')
+      // );
+      if (
+        scrollDirection.current === 'down' &&
+        currentScrollDistance.current > 100 &&
+        window.innerWidth < 748 &&
+        blockRef!.current!.getAttribute('data-is-open') === 'false'
+      ) {
+        console.log('hide header');
+        blockRef!.current!.style.opacity = '0';
+      } else if (
+        scrollDirection.current === 'up' &&
+        currentScrollDistance.current > 100 &&
+        window.innerWidth < 748 &&
+        blockRef!.current!.getAttribute('data-is-open') === 'false'
+      ) {
+        console.log('show header');
+        blockRef!.current!.style.opacity = '1';
+      }
+    };
+    const onScrollEnd = () => {
+      scrolledFromTop.current = window.scrollY;
+      console.log('scrolledFromTop', scrolledFromTop.current);
+      console.log('scrollDirection', scrollDirection.current);
+    };
+    const onResize = () => {
+      blockRef!.current!.style.opacity = '1';
+    };
+    addEventListener('scroll', onScroll);
+    addEventListener('scrollend', onScrollEnd);
+    addEventListener('resize', onResize);
+    return () => {
+      removeEventListener('scroll', onScroll);
+      removeEventListener('scrollend', onScrollEnd);
+      removeEventListener('resize', onResize);
+    };
+  }, []);
+
   return (
-    <header className={`${style.header} ${isOpen && style.active}`}>
+    <header
+      ref={blockRef}
+      data-is-open={isOpen}
+      className={`${style.header} ${isOpen && style.active}`}>
+      <Image className={style.logo} src={logo} alt={'logo'} />
       <div className={style.links}>
         {routes.map(({ link, text }) => (
           <NavLink
@@ -32,9 +102,7 @@ export const Header = () => {
           </NavLink>
         ))}
       </div>
-      <div
-        onClick={() => setIsOpen(!isOpen)}
-        className={`${style.burger} ${isOpen && style.active}`}>
+      <div onClick={() => setIsOpen(!isOpen)} className={style.burger}>
         <span></span>
       </div>
     </header>
