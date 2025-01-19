142b75a98c98e5306ffc33848f7aac53ea98eebb
Uladzislau Papliouka <uladzislau.papliouka@gmail.com>
2025-01-19 19:06:23 +0300

    feat(layout): add minimal mobile adaptive layout

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,15 +2,41 @@ import { usePathname } from 'next/navigation';
 import { NavLink } from './NavLink';
 import style from './index.module.scss';
 import { routes } from 'shared/routes';
+import { useEffect, useState } from 'react';
 export const Header = () => {
   const currentPath = usePathname();
+  const [isOpen, setIsOpen] = useState(false);
+  useEffect(() => {
+    const onResize = () => {
+      if (window.innerWidth > 700) {
+        setIsOpen(false);
+      }
+    };
+    addEventListener('resize', onResize);
+    return () => {
+      removeEventListener('resize', onResize);
+    };
+  }, []);
   return (
-    <header className={style.header}>
-      {routes.map(({ link, text }) => (
-        <NavLink key={link} href={link} active={currentPath === link}>
-          {text}
-        </NavLink>
-      ))}
+    <header className={`${style.header} ${isOpen && style.active}`}>
+      <div className={style.links}>
+        {routes.map(({ link, text }) => (
+          <NavLink
+            onClick={() => {
+              setIsOpen(false);
+            }}
+            key={link}
+            href={link}
+            active={currentPath === link}>
+            {text}
+          </NavLink>
+        ))}
+      </div>
+      <div
+        onClick={() => setIsOpen(!isOpen)}
+        className={`${style.burger} ${isOpen && style.active}`}>
+        <span></span>
+      </div>
     </header>
   );
 };
